d51c5dd000e95776c0091cf1f878a7e2c7a9c2ff
Cam <camglowicki@hotmail.com>
2021-04-14 09:18:43 -0700

    initial commit; project setup and clean slate layout

---
+++ src/components/navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Navigation() {
+  return (
+    <nav className="site-navigation">
+      <span>Langauge Keepers</span>
+      <ul>
+        <li>
+          1
+        </li>
+      </ul>
+    </nav>
+  );
+} 
