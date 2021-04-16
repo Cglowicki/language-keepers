50d3e7f1b84b6a75c0b2fa8b820aa4dd7ad68444
Cam <camglowicki@hotmail.com>
2021-04-15 17:55:30 -0700

    set up app router for pages

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React from 'react';
 import Navigation from './components/navigation';
 import { BrowserRouter as Router, Swtich, Route, Redirect } from 'react-router-dom';
 
+import PageRenderer from './page-renderer';
+
 function App() {
   return (
     <Router>
       <div className="App">
         <Navigation />
+        <Swtich>
+          <Route path="/:page" component={PageRenderer} />
+          <Route path="/" render={() => <Redirect to="/home" />} />
+          <Route component={() => 404} />
+        </Swtich>
       </div>
     </Router>
   );
