d2072389657eafbe4885a335f6c941a6e83cad50
Cam <camglowicki@hotmail.com>
2021-04-17 15:06:04 -0700

    nav icon for respnsive design

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -23,14 +23,15 @@ const navLinks = [
 export default function Navigation() {
   return (
     <nav className="site-navigation">
-      <span>Langauge Keepers</span>
+      <span className="menu-title">Langauge Keepers</span>
       <ul>
         {navLinks.map((link, index) => (
           <li key={index}>
             <Link to={link.path}>{link.title}</Link>
           </li>
         ))
-        }
+      }
+      <i className="fas fa-bars"></i>
       </ul>
     </nav>
   );
