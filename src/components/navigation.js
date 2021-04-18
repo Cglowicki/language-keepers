193b6c847be05bf9f36ac105ef58b4aeacfc99ec
Cam <camglowicki@hotmail.com>
2021-04-17 18:01:26 -0700

    initial nav icon styling

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState, setState } from 'react';
+import { Link } from 'react-router-dom';
 
 const navLinks = [
   {
@@ -21,18 +21,23 @@ const navLinks = [
 ];
 
 export default function Navigation() {
+  //responsive nav menu
+  const [menuActive, setMenuActive] = useState(false);
+
   return (
     <nav className="site-navigation">
       <span className="menu-title">Langauge Keepers</span>
-      <ul>
-        {navLinks.map((link, index) => (
-          <li key={index}>
-            <Link to={link.path}>{link.title}</Link>
-          </li>
-        ))
-      }
-      <i className="fas fa-bars"></i>
-      </ul>
+      <div className="menu-content-container">
+        <ul>
+          {navLinks.map((link, index) => (
+            <li key={index}>
+              <Link to={link.path}>{link.title}</Link>
+            </li>
+          ))
+          }
+        </ul>
+        <i className="fas fa-bars" onClick={() => setMenuActive(!menuActive)} />
+      </div>
     </nav>
   );
 } 
