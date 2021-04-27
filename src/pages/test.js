1324a35e377ecc70a82a80c99903201827038cce
Cam <camglowicki@hotmail.com>
2021-04-27 06:42:11 -0700

    target scss for audio player

--- src/pages/test.js
+++ src/pages/test.js
@@ -2,11 +2,13 @@ import React from 'react';
 import pic from '../assets/pdf_pics/test-slide.jpeg';
 import aud from '../assets/pdf_pics/test-aud.mp3';
 
+import '../assets/scss/test.scss';
+
 const testPDF = () => {
   return (
     <div className="aud-vis">
       <audio controls>
-        <source src={aud} type="audio/mpeg"></source>
+        <source className="test-aud-controls" src={aud} type="audio/mpeg"></source>
       </audio>
       <img src={pic} alt="test"></img>
       <div>this is testing aud vis</div>
