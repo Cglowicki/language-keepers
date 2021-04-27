d9cd4a2294cc1af5d852de5ef8974a1b25bba9c4
Cam <camglowicki@hotmail.com>
2021-04-27 09:31:08 -0700

    attempts to load pdf poster into look page

--- src/pages/look.js
+++ src/pages/look.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Document } from 'react-pdf';
+import goodRoots from '../assets/pdf_pics/good-roots.pdf';
 
 
 
 export default function Posters() {
   return (
-    <div>look</div>
+    <div>look
+
+      <Document file={goodRoots}>
+        
+        </Document>
+
+</div>
   );
 }
