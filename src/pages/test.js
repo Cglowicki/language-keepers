25c8accc2dd75ccd05bcf336de2b73ef4bbaa4bc
Cam <camglowicki@hotmail.com>
2021-04-27 06:01:20 -0700

    test aud and vis for pdfs

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,10 +1,15 @@
 import React from 'react';
-//import SamplePDF from '../assets/files/LG1.pdf';
+import pic from '../assets/pdf_pics/test-slide.jpeg';
+import aud from '../assets/pdf_pics/test-aud.mp3';
 
 const testPDF = () => {
   return (
-    <div className="test-pdf">
-      {/* <SamplePDF /> */}
+    <div className="aud-vis">
+      <audio controls>
+        <source src={aud} type="audio/mpeg"></source>
+      </audio>
+      <img src={pic} alt="test"></img>
+      <div>this is testing aud vis</div>
     </div>
   );
 }
